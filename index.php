6c4156154a831d4ea4a02d469cb4c4ee82dfdf65
Yusto <onesmusmakori@gmail.com>
2017-07-23 17:23:10 +0300

    this

---
+++ index.php
@@ -0,0 +1,24 @@
+<Html>
+<head>
+<title>Lets Make Peace</title>
+
+<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body >
+
+<div class="left">
+<form action="join.php" method="post">
+<h4>Join The peace campaign</h4>
+
+<input type="text" name="username"/><br>
+<input type="password" name="password"/><br>
+<input type="submit" name ="login"/><br>
+
+</form>
+</div>
+
+
+
+</body>
+</html>
